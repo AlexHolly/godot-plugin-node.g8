156a29b41b6b73f384e47b9483abc67ec127eee1
Alexander Holland <alexander.holland@live.de>
2016-06-20 14:20:57 +0200

    fix lower char

--- src/main/g8/$name$_plugin.gd
+++ src/main/g8/$name$_plugin.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 func _enter_tree():
 	# When this plugin node enters tree, add the custom type
 
-	add_custom_type("$name$","$rootnode$",preload("res://addons/$name$/$name$.gd"),preload("res://addons/$name$/node_icon.png"))
+	add_custom_type("$name$","$rootnode$",preload("res://addons/$name;format="lower"$/$name$.gd"),preload("res://addons/$name;format="lower"$/node_icon.png"))
 
 func _exit_tree():
 	# When the plugin node exits the tree, remove the custom type
